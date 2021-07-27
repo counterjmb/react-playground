a1d347655f255a77178e2b75251da6dbef4eae06
jbeagle <James.Beagle@nelnet.net>
2021-07-27 15:13:28 -0500

    commiterino

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import NestedRouting from './nestedrouting/NestedRouting';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
